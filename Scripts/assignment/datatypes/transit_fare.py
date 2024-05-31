c96c192e8d69a8d0c33de504c2b99955e91a1825
Jarkko Jalovaara <jarkko.jalovaara@hotmail.fi>
2024-05-31 14:55:31 +0300

    allow .tco files without distance column

--- Scripts/assignment/datatypes/transit_fare.py
+++ Scripts/assignment/datatypes/transit_fare.py
@@ -13,20 +13,29 @@ class TransitFareZoneSpecification:
             Table of transit zone combination fares
         """
 
-        default_rows: Dict = fare_table["fare"].to_dict()
+        if 'dist' not in fare_table.columns: 
+            self.zone_fares: Dict = fare_table["fare"].to_dict()
+            try:
+                self.exclusive: Dict = fare_table["exclusive"].dropna().to_dict()
+            except KeyError:
+                self.exclusive = {}
+            self.dist_fare: float = self.zone_fares.pop("dist")
+        
+        else:
+            default_rows: Dict = fare_table["fare"].to_dict()
 
-        distance_rows = fare_table[(fare_table["dist"] != 0)].index
-        zone_rows = fare_table[(fare_table["dist"] == 0)].index
+            distance_rows = fare_table[(fare_table["dist"] != 0)].index
+            zone_rows = fare_table[(fare_table["dist"] == 0)].index
 
-        self.zone_fares: Dict = fare_table["fare"].drop(distance_rows).to_dict()
-        try:
-            self.exclusive: Dict = fare_table["exclusive"].dropna().to_dict()
-        except KeyError:
-            self.exclusive = {}
-        self.distance_fares: Dict = fare_table[["fare","dist"]].drop(zone_rows).to_dict('index')
+            self.zone_fares: Dict = fare_table["fare"].drop(distance_rows).to_dict()
+            try:
+                self.exclusive: Dict = fare_table["exclusive"].dropna().to_dict()
+            except KeyError:
+                self.exclusive = {}
+            self.distance_fares: Dict = fare_table[["fare","dist"]].drop(zone_rows).to_dict('index')
 
-        self.default_dist_fare: float = default_rows.pop("dist")
-        self.default_start_fare: float = default_rows.pop("start")
+            self.default_dist_fare: float = default_rows.pop("dist")
+            self.default_start_fare: float = default_rows.pop("start")
 
     @property
     def transit_fare_zones(self) -> Set[str]:
